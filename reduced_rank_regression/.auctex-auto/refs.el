cf9cdf862667c61661537a6db332e7ad798372cd
Parker Knight <plb.knight@gmail.com>
2022-04-09 22:02:07 -0400

    start sparsity notes

---
+++ reduced_rank_regression/.auctex-auto/refs.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "refs"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "witten_penalized_2009"
+    "allen_generalized_2014"
+    "wang_generalized_2021"
+    "izenman_reduced-rank_1975"))
+ :bibtex)
+
